367aa050016b853578170bb2e2a0a4e129c10e1e
Sibiraj PR <sibizulu@gmail.com>
2016-09-19 12:24:08 +0530

    Adding profile name

--- sites/all/themes/kgtech/template.php
+++ sites/all/themes/kgtech/template.php
@@ -9,3 +9,21 @@ global $user;
 
   }
 }
+
+
+function kgtech_menu_alter(&$items) {
+    // Change the title of user profile pages to the user's name.  Gak.
+    $items['user/%user']['title callback'] = 'kgtech_user_page_title';
+}
+
+function kgtech_user_page_title() {
+    if (arg(0) == 'user') {
+        $user_data = user_load(arg(1));
+        $output = $user_data->field_name['und'][0]['value'];
+    }
+    // Fallback to username if no fields are present
+    if (empty($output)) {
+        $output = $user->name;
+    }
+    return $output;
+}
