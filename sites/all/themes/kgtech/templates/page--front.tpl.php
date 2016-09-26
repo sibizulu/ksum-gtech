26e54a308bc2d1643e8a6d17e654907721cff787
Sibiraj PR <sibizulu@gmail.com>
2016-09-26 16:07:07 +0530

    Adding page tpls

---
+++ sites/all/themes/kgtech/templates/page--front.tpl.php
@@ -0,0 +1,119 @@
+<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
+  <div class="<?php print $container_class; ?>">
+    <div class="navbar-header">
+      <?php if ($logo): ?>
+        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
+
+      <?php if (!empty($site_name)): ?>
+        <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+      <?php endif; ?>
+
+      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      <?php endif; ?>
+    </div>
+
+    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+      <div class="navbar-collapse collapse">
+        <nav role="navigation">
+
+     <?php
+        if (module_exists('hybridauth') && user_is_anonymous()) {
+          $element['#type'] = 'hybridauth_widget';
+          print drupal_render($element);
+        }
+      ?>
+          <?php if (!empty($primary_nav)): ?>
+            <?php print render($primary_nav); ?>
+          <?php endif; ?>
+          <?php if (!empty($secondary_nav)): ?>
+            <?php print render($secondary_nav); ?>
+          <?php endif; ?>
+          <?php if (!empty($page['navigation'])): ?>
+            <?php print render($page['navigation']); ?>
+          <?php endif; ?>
+
+        </nav>
+      </div>
+    <?php endif; ?>
+  </div>
+</header>
+
+<header role="banner" id="page-header" class="banner-area">
+  <?php if (!empty($site_slogan)): ?>
+    <p class="lead"><?php print $site_slogan; ?></p>
+  <?php endif; ?>
+
+  <?php print render($page['header']); ?>
+</header> <!-- /#page-header -->
+
+<div class="main-container container-fluid">
+
+  <div class="row">
+
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <aside class="col-sm-3" role="complementary">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>  <!-- /#sidebar-first -->
+    <?php endif; ?>
+
+    <section<?php print $content_column_class; ?>>
+      <?php if (!empty($page['highlighted'])): ?>
+        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
+      <?php endif; ?>
+      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if (!empty($title)): ?>
+        <h1 class="page-header"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+      <?php endif; ?>
+      <?php if (!empty($page['help'])): ?>
+        <?php print render($page['help']); ?>
+      <?php endif; ?>
+      <?php if (!empty($action_links)): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php //print render($page['content']); ?>
+
+ <p>
+ Rapidiously benchmark e-business technologies and go forward bandwidth. Interactively repurpose distributed applications for fully tested ideas. Appropriately engineer intermandated opportunities with reliable e-markets. Holisticly pontificate low-risk high-yield quality vectors before cross-platform solutions. Assertively harness exceptional strategic theme areas before wireless convergence.
+
+Rapidiously mesh market positioning human capital after fully researched synergy. Rapidiously empower 2.0 portals before sustainable metrics. Appropriately foster user friendly vortals via premier channels. Phosfluorescently deploy standards compliant quality vectors without customer directed information. Seamlessly redefine installed base deliverables after plug-and-play materials.
+
+Continually underwhelm resource sucking architectures rather than viral technology. Seamlessly aggregate top-line systems and.
+</p>
+
+<p>Interactively evolve fully tested relationships whereas market-driven interfaces. Compellingly redefine sticky imperatives without impactful web services. Conveniently streamline compelling supply chains rather than future-proof growth strategies. Intrinsicly deploy multifunctional results without prospective resources. Collaboratively enable integrated methodologies before innovative metrics.
+
+Interactively benchmark parallel web services through team driven technology. Rapidiously enable market positioning customer service before process-centric solutions. Dynamically utilize resource sucking channels for maintainable products. Enthusiastically facilitate excellent communities through covalent action items. Enthusiastically conceptualize quality process improvements and web-enabled synergy.
+
+Monotonectally drive pandemic methodologies vis-a-vis cutting-edge "outside the box" thinking. Conveniently harness sticky platforms after sustainable.</p>
+    </section>
+
+    <?php if (!empty($page['sidebar_second'])): ?>
+      <aside class="col-sm-3" role="complementary">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>  <!-- /#sidebar-second -->
+    <?php endif; ?>
+
+  </div>
+</div>
+
+<?php if (!empty($page['footer'])): ?>
+  <footer class="footer container-fluid">
+    <?php print render($page['footer']); ?>
+  </footer>
+<?php endif; ?>
